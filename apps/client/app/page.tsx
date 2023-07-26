da4b5ad06b232f13ca364452b4ff713faffe57ad
antonpalermo <anpbusinessmail@gmail.com>
2023-07-26 21:57:59 +0800

    init tailwindcss

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -4,5 +4,5 @@ import { db } from "database"
 export default async function Home() {
   const names = await db.selectFrom("sample").select(["id", "name"]).execute()
 
-  return <div>{JSON.stringify(names)}</div>
+  return <div className="text-green-500">{JSON.stringify(names)}</div>
 }

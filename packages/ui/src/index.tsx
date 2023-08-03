9cca8e16b148467c1e30649cc0724c53ceeef032
antonpalermo <anpbusinessmail@gmail.com>
2023-08-03 21:53:38 +0800

    added shadcn-ui components

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,5 +1,9 @@
 export * from "./lib/utils"
+
+export * from "./components/label"
+export * from "./components/input"
 export * from "./components/button"
 export * from "./components/dialog"
 export * from "./components/command"
 export * from "./components/popover"
+export * from "./components/textarea"

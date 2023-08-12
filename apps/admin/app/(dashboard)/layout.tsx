2c44681690a91ba1ddbbc9dd76cf210edcc24226
antonpalermo <anpbusinessmail@gmail.com>
2023-08-12 22:35:39 +0800

    fix: redirect fallback
    added fallback redirects and remove default redirect to overview that
    returns a 404 error.

--- apps/admin/app/(dashboard)/layout.tsx
+++ apps/admin/app/(dashboard)/layout.tsx
@@ -27,7 +27,7 @@ export default async function DashboardLayout({
     .executeTakeFirst()
 
   if (!store) {
-    redirect("/store-setup")
+    redirect("/")
   }
 
   return (
--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -2,9 +2,6 @@
 const config = {
   reactStrictMode: true,
   transpilePackages: ["ui"],
-  redirects: async () => [
-    { source: "/", destination: "/overview", permanent: false }
-  ]
 }
 
 module.exports = config

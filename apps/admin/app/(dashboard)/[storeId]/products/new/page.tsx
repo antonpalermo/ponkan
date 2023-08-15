e84c733334f0c8aa80b702d06eb92fa2b119afa6
antonpalermo <anpbusinessmail@gmail.com>
2023-08-15 22:46:43 +0800

    feat: create products
    user can now create products for a store

---
+++ apps/admin/app/(dashboard)/[storeId]/products/new/page.tsx
@@ -0,0 +1,10 @@
+import ProductForm from "@/components/products/product-form"
+
+export default async function NewProductPage() {
+  return (
+    <div>
+      <h1>Add new product</h1>
+      <ProductForm />
+    </div>
+  )
+}
---
+++ apps/admin/app/(dashboard)/[storeId]/products/page.tsx
@@ -0,0 +1,8 @@
+export default async function ProductsPage() {
+  return (
+    <div>
+      <h1>Products</h1>
+      <p>All products currently on stock</p>
+    </div>
+  )
+}

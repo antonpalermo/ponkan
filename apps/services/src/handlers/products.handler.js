73c4a457d50da5fb4a0237c066a2d9760e15ab7c
antonpalermo <anpbusinessmail@gmail.com>
2023-09-26 17:54:58 +0800

    feat: added mime type validation

--- apps/services/src/handlers/products.handler.js
+++ apps/services/src/handlers/products.handler.js
@@ -1,5 +1,4 @@
 async function upload(req, res) {
-  console.log("handler: ", req.files)
   return res.status(200).json({ message: "upload ok" })
 }
 
--- apps/services/src/middlewares/products.middleware.js
+++ apps/services/src/middlewares/products.middleware.js
@@ -1,7 +1,21 @@
 const { validateBufferMIMEType } = require("validate-image-type")
 
 async function validateMimeType(req, res, next) {
-  console.log("middleware: ", req.files)
+  if (!req.files) {
+    throw new Error("No files attached")
+  }
+
+  for (file of req.files) {
+    const buffer = await validateBufferMIMEType(file.buffer, {
+      originalFilename: file.originalname,
+      allowMimeTypes: ["image/png", "image/jpg", "image/jpeg"]
+    })
+
+    if (!buffer.ok) {
+      return res.status(400).json({ message: "invalid file" })
+    }
+  }
+
   next()
 }
 

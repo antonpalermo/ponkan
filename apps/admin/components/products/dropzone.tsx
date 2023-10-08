1081d708f88e2996949297e792e78e4e7e506d8e
antonpalermo <anpbusinessmail@gmail.com>
2023-10-08 22:03:59 +0800

    added file preview

--- apps/admin/components/products/dropzone.tsx
+++ apps/admin/components/products/dropzone.tsx
@@ -1,18 +1,31 @@
 "use client"
 
-import { Fragment, useCallback } from "react"
+import { FormEvent, useCallback, useState } from "react"
 import { useDropzone } from "react-dropzone"
 import { Input, Label } from "ui"
+import Image from "next/image"
 
 export default function ProductImageDropzone() {
-  const onDrop = useCallback(files => {
-    console.log(files)
+  const [filePreview, setFilePreview] = useState<{ preview: string }[]>([])
+
+  const onDrop = useCallback((acceptedFiles: File[]) => {
+    acceptedFiles.forEach(file => {
+      setFilePreview(prev => [...prev, { preview: URL.createObjectURL(file) }])
+    })
   }, [])
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
+  async function onSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    const formData = new FormData()
+
+    console.log(filePreview)
+    console.log(formData)
+  }
+
   return (
-    <Fragment>
+    <form onSubmit={onSubmit}>
       <Label>Product Image</Label>
       <div
         {...getRootProps({
@@ -27,6 +40,13 @@ export default function ProductImageDropzone() {
           <p>Drag 'n' drop some files here, or click to select files</p>
         )}
       </div>
-    </Fragment>
+      <ul className="inline-flex space-x-3">
+        {filePreview.map(url => (
+          <li key={url.preview}>
+            <Image src={url.preview} width={100} height={100} alt="image" />
+          </li>
+        ))}
+      </ul>
+    </form>
   )
 }
